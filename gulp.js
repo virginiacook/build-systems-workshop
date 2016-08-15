d79bd098de7fc0dd99298c979c987ef00e119a14
virginiacook <virg.cook@earthlink.net>
2016-08-15 17:10:20 -0400

    insert your commit message here

---
+++ gulp.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var argv = require('yargs').argv;
+var git = require('gulp-git');
+var runSequence = require('run-sequence');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
